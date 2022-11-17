3292268c95029b71801c656824331949aebc9878
pradeepkumar170301 <pradeepkumar@nmsworks.co.in>
2022-11-17 12:51:12 +0530

    change hello to addition program

--- First.java
+++ First.java
@@ -1,5 +1,8 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int a=1;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
     }
 }
