b7edfee7a5919489ea9b057fdca7b551c02e9346
pradeepkumar170301 <pradeepkumar@nmsworks.co.in>
2022-11-17 15:17:32 +0530

    multiplication will be added

--- Sub.java
+++ Sub.java
@@ -2,7 +2,7 @@ public class Sub {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        int c = a - b;
+        int c = a * b;
         System.out.println("C = " + c);
     }
 }
